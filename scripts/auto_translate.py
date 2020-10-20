e709b2ab480033d148393ea709532300d2c32b6c
Hallis - home main <Hallis1221_main@users.noreply.github.com>
2020-10-20 18:18:54 +0200

    Summary

--- scripts/auto_translate.py
+++ scripts/auto_translate.py
@@ -1,9 +1,23 @@
 from googletrans import Translator
+translator = Translator()
 
 enUS_path = "en_US.lang"
+destinations = ["us","gb","de","es","mx"]
+rawTranslations = []
 
-for line in open(enUS_path, "R"):
-    print(line)
-    
-translator = Translator()
-print(translator.translate("Hei verden!", dest='sw'))
+for line in open(enUS_path):
+    lineToRemove = ""
+    for c in line:
+        lineToRemove += c
+        if c == "=":
+            break
+    rawTranslations.append(line.strip(lineToRemove))
+
+for i in range(0, len(destinations)):
+    try:
+        translations = translator.translate(rawTranslations, dest=destinations[i])
+    except ValueError:
+        print(destinations[i], "is a invalid lang code for google translate")
+
+    for translation in translations:
+        print(translation.text)
